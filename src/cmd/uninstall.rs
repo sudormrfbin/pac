maralla/pack

commit 7fddae180c3f37d65024bdf4989871d27c304bd1

Show error when uninstalling invalid package

src/cmd/uninstall.rs
@@ -1,5 +1,5 @@
 use crate::package::{self, Package};
-use crate::Result;
+use crate::{Error, Result};
 
 use clap::ArgMatches;
 use std::fs;
@@ -29,8 +29,19 @@ pub fn exec(matches: &ArgMatches) {
 
 fn uninstall_plugins(plugins: &[String], all: bool) -> Result<()> {
     let mut packs = package::fetch()?;
+    let mut packs_iter = packs.iter();
 
-    for pack in packs.iter().filter(|p| plugins.contains(&p.name)) {
+    let to_uninstall = plugins
+        .iter()
+        .map(
+            |plugin| match packs_iter.find(|pack| &pack.name == plugin) {
+                Some(p) => Ok(p),
+                None => Err(Error::plugin_not_installed(plugin)),
+            },
+        )
+        .collect::<Result<Vec<&Package>>>()?;
+
+    for pack in to_uninstall {
         uninstall_plugin(pack, all)?;
     }
 

sudormrfbin/pac

commit d13603ff560cb913d9cc576e037a4da94e71eb00

Correctly uninstall multiple packages
Package names are checked before uninstalling them so that
invalid names show an error and exit. Only the first name
was being checked due to reusing a consumed iterator and
this gave a false positive of an invalid plugin.

Bug introduced by 7fddae180c3f3

src/cmd/uninstall.rs
@@ -31,12 +31,11 @@ pub fn exec(matches: &ArgMatches) {
 /// purges related plugin specific config file.
 fn uninstall_plugins(plugins: &[String], all: bool) -> Result<()> {
     let mut packs = package::fetch()?;
-    let mut packs_iter = packs.iter();
 
     let to_uninstall = plugins
         .iter()
         .map(
-            |plugin| match packs_iter.find(|pack| &pack.name == plugin) {
+            |plugin| match packs.iter().find(|pack| &pack.name == plugin) {
                 Some(p) => Ok(p),
                 None => Err(Error::plugin_not_installed(plugin)),
             },